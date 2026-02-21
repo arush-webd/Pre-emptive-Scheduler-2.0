5810b14f3f4393ea10add4c897de7832d05d555a
Arush Bhatnagar <148698224+arush-webd@users.noreply.github.com>
2026-02-21 08:10:07 +0530

    Create sync.h

---
+++ sync.h
@@ -0,0 +1,34 @@
+#ifndef SYNC_H
+#define SYNC_H
+
+#include "queue.h"
+#include "scheduler.h"
+
+typedef struct {
+    int value;
+    queue_t waiters;
+} sem_t;
+
+typedef struct {
+    queue_t waiters;
+} cond_t;
+
+typedef struct {
+    int total;
+    int count;
+    queue_t waiters;
+} barrier_t;
+
+void sem_init(sem_t *s, int initial);
+void sem_wait(sem_t *s);
+void sem_signal(sem_t *s);
+
+void cond_init(cond_t *c);
+void cond_wait(cond_t *c);
+void cond_signal(cond_t *c);
+void cond_broadcast(cond_t *c);
+
+void barrier_init(barrier_t *b, int total_threads);
+void barrier_wait(barrier_t *b);
+
+#endif
