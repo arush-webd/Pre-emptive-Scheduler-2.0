db04e649b61283a3d8b256764020487b4ebe0645
Arush Bhatnagar <148698224+arush-webd@users.noreply.github.com>
2026-02-24 04:38:11 +0530

    Update interrupt.h

--- interrupt.h
+++ interrupt.h
@@ -3,12 +3,20 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void enter_critical(void);
 void leave_critical(void);
 
 void idt_init(void);
 void pic_remap(void);
 void pit_init(uint32_t hz);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 static inline void sti(void) { __asm__ volatile ("sti"); }
 static inline void cli(void) { __asm__ volatile ("cli"); }
