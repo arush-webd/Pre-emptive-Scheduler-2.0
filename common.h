5a9c88140b609197ff1be6ae8b65c5e82b0f8bb8
Arush Bhatnagar <148698224+arush-webd@users.noreply.github.com>
2026-02-24 04:38:34 +0530

    Update common.h

--- common.h
+++ common.h
@@ -3,22 +3,22 @@
 
 #include <stdint.h>
 
-/* ----- PCB layout must match entry.S expectations ----- */
-/* entry.S uses:
- *  - SAVE_STACK: movl %esp, (%eax)  => pcb->saved_esp at offset 0
+/*
+ * ----- PCB layout must match entry.s expectations -----
+ * entry.s uses:
+ *  - SAVE_STACK: movl %esp, (%eax) => pcb->saved_esp at offset 0
  *  - TEST_NESTED_COUNT: NESTED_COUNT_OFFSET(%eax)
  */
 #define NESTED_COUNT_OFFSET 16
 
-/* ----- I/O port helpers ----- */
 static inline void outb(uint8_t val, uint16_t port) {
-    __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+	__asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
 static inline uint8_t inb(uint16_t port) {
-    uint8_t ret;
-    __asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
+	uint8_t ret;
+	__asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+	return ret;
 }
 
 #endif
