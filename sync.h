13efd61dacfbcf5366a89ea2e8fbe03a09cd8500
Arush Bhatnagar <148698224+arush-webd@users.noreply.github.com>
2026-02-24 04:39:33 +0530

    Update sync.h

--- sync.h
+++ sync.h
@@ -5,19 +5,23 @@
 #include "scheduler.h"
 
 typedef struct {
-    int value;
-    queue_t waiters;
+	int value;
+	queue_t waiters;
 } sem_t;
 
 typedef struct {
-    queue_t waiters;
+	queue_t waiters;
 } cond_t;
 
 typedef struct {
-    int total;
-    int count;
-    queue_t waiters;
+	int total;
+	int count;
+	queue_t waiters;
 } barrier_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void sem_init(sem_t *s, int initial);
 void sem_wait(sem_t *s);
@@ -30,5 +34,9 @@ void cond_broadcast(cond_t *c);
 
 void barrier_init(barrier_t *b, int total_threads);
 void barrier_wait(barrier_t *b);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif
