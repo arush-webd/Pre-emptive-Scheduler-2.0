89e53337f59b16c46caf4b94b77ded8337ca1cc6
Arush Bhatnagar <148698224+arush-webd@users.noreply.github.com>
2026-02-24 22:37:59 +0530

    Update interrupt.h

--- interrupt.h
+++ interrupt.h
@@ -1,24 +1,26 @@
-#ifndef INTERRUPT_H
-#define INTERRUPT_H
-
+#pragma once
 #include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+static inline void outb(uint16_t port, uint8_t val) {
+  __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+static inline uint8_t inb(uint16_t port) {
+  uint8_t ret;
+  __asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+  return ret;
+}
 
-void enter_critical(void);
-void leave_critical(void);
+static inline void sti(void){ __asm__ volatile("sti"); }
+static inline void cli(void){ __asm__ volatile("cli"); }
 
 void idt_init(void);
 void pic_remap(void);
 void pit_init(uint32_t hz);
 
-#ifdef __cplusplus
-} // extern "C"
-#endif
+void irq0_install(void);
 
-static inline void sti(void) { __asm__ volatile ("sti"); }
-static inline void cli(void) { __asm__ volatile ("cli"); }
+/* called from assembly */
+uint32_t irq0_c(uint32_t old_esp);
 
-#endif
+/* debug tick counter */
+extern volatile uint64_t time_elapsed;
